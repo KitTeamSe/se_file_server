550c632eb7201d463e0135e378b12d0d7d3e4c9a
dubu4050 <dubu4050@naver.com>
2021-07-27 18:41:04 +0900

    Feat: 페이징 정렬 기준 추가
    - 정렬 기준 속성 props 추가
    
    # KIT-65

--- src/main/java/com/se/fileserver/v1/common/application/dto/request/PaginationRequest.java
+++ src/main/java/com/se/fileserver/v1/common/application/dto/request/PaginationRequest.java
@@ -1,39 +1,33 @@
-package com.se.fileserver.v1.common.application.dto;
+package com.se.fileserver.v1.common.application.dto.request;
 
 import io.swagger.annotations.ApiModelProperty;
 import javax.validation.constraints.Min;
+import lombok.Getter;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 
-public class PaginationRequest<T> extends CommonRequest<T>{
+@Getter
+public class PaginationRequest<T> extends BaseRequest<T> {
   @ApiModelProperty(example = "1", notes = "페이지, 1 이상만 가능")
   @Min(value = 1)
   private int page;
   @ApiModelProperty(example = "50", notes = "페이지의 사이즈, 10 이상 50 이하만 가능")
   @Min(value = 10)
   private int size;
-  @ApiModelProperty(example = "ASC", notes = "정렬 방향, 기준(생성일)")
+  @ApiModelProperty(example = "ASC", notes = "정렬 방향")
   private Sort.Direction direction;
+  @ApiModelProperty(example = "id", notes = "정렬 기준")
+  private String props;
 
   public PaginationRequest() {
   }
 
-  public PaginationRequest(T dto, int page, int size, Direction direction) {
+  public PaginationRequest(T dto, int page, int size,
+      Direction direction, String props) {
     super(dto);
     this.page = page;
     this.size = size;
     this.direction = direction;
-  }
-
-  public int getPage() {
-    return page;
-  }
-
-  public int getSize() {
-    return size;
-  }
-
-  public Direction getDirection() {
-    return direction;
+    this.props = props;
   }
 }
