a8a99b584f46aab819731d58499f5cf4e426e866
dubu4050 <dubu4050@naver.com>
2021-07-28 18:53:01 +0900

    Fix: Dto Validation
    - dto의 각 필드 validation 추가
    - nested Object validation 설정
    
    # KIT-65

--- src/main/java/com/se/fileserver/v1/account/application/dto/AccountSignInDto.java
+++ src/main/java/com/se/fileserver/v1/account/application/dto/AccountSignInDto.java
@@ -1,5 +1,8 @@
 package com.se.fileserver.v1.account.application.dto;
 
+import javax.persistence.Column;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,7 +10,9 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AccountSignInDto {
+  @Size(min = 4, max = 20)
   private String id;
+  @NotBlank
   private String password;
 
   public AccountSignInDto(String id, String password) {
--- src/main/java/com/se/fileserver/v1/common/application/dto/request/BaseRequest.java
+++ src/main/java/com/se/fileserver/v1/common/application/dto/request/BaseRequest.java
@@ -2,10 +2,12 @@ package com.se.fileserver.v1.common.application.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import io.swagger.annotations.ApiModelProperty;
+import javax.validation.Valid;
 
 public class BaseRequest<T> {
   @JsonUnwrapped
   @ApiModelProperty
+  @Valid
   private T dto;
 
   public BaseRequest() {
