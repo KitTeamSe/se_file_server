52e6daefdf66c4a7f34b75f1fa405c798943cb85
dldhk97 <dldhk97@gmail.com>
2021-09-08 17:55:02 +0900

    Resolve CorsError

--- src/main/java/com/se/fileserver/v1/common/infra/web/filter/CorsHeaderFilter.java
+++ src/main/java/com/se/fileserver/v1/common/infra/web/filter/CorsHeaderFilter.java
@@ -11,6 +11,9 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsHeaderFilter {
 
+  @Value("${se-file-server.domain}")
+  private String fileServerDomain;
+
   @Value("${se-web-server.file-dash-server-domain}")
   private String fileDashServerDomain;
 
@@ -20,6 +23,7 @@ public class CorsHeaderFilter {
     CorsConfiguration config = new CorsConfiguration();
     config.setAllowCredentials(true);
     config.addAllowedOrigin("http://localhost:3000");
+    config.addAllowedOrigin(fileServerDomain);
     config.addAllowedOrigin(fileDashServerDomain);
     config.addAllowedHeader("*");
     config.addAllowedMethod("*");
